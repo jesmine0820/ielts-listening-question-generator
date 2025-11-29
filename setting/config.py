b9e5fba41cc685ba409a363dc85204ff370675a1
jesmine <khaijingtey0820@gmail.com>
2025-11-29 16:00:40 +0800

    Update database

--- setting/config.py
+++ setting/config.py
@@ -1,1 +1,2 @@
-API_KEY = "AIzaSyCSC0LPUznCj0USGxAVXjXT_4vgVqp-ah4"
+API_KEY = "AIzaSyCSC0LPUznCj0USGxAVXjXT_4vgVqp-ah4"  # Database
+API_KEY2 = "AIzaSyBS-2pbdjYouOkcqHaX4ZI5HHPpSSmq3iw" # Question Generator
