52359e03d10a3a823552d91e70fe059452f032d5
jesmine <khaijingtey0820@gmail.com>
2025-12-24 08:47:29 +0800

    Update ffmpeg

--- app.py
+++ app.py
@@ -596,4 +596,8 @@ def submit_feedback():
 
 # Main
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
